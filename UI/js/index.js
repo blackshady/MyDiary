037b3657375c332d6207157bbd04bdcc2b903d49
Victor Chibuike <victorblackshady@gmail.com>
2018-07-08 09:41:29 +0100

    [#158885569]-align form to left

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,3 +1,7 @@
-class Animation {
+const signupBtn = document.getElementsByName(".btn__signup")[0];
+
+if (signupBtn) {
+  signupBtn.addEventListener("click", () =>
+    console.log(signupBtn))
 
 }
