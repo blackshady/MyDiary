b0f4a03569b0d1091ea4d1ae05c493510522a2c5
Victor Chibuike <victorblackshady@gmail.com>
2018-07-08 20:49:43 +0100

    [finishes #158885569] done with sign up page

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,7 +1,32 @@
-const signupBtn = document.getElementsByName(".btn__signup")[0];
+let userDetails = [];
 
-if (signupBtn) {
-  signupBtn.addEventListener("click", () =>
-    console.log(signupBtn))
 
+const inputLabel = Array.from((document.getElementsByClassName("input__label")));
+const inputField = Array.from((document.getElementsByClassName("input__field")));
+const siginUpBtn = document.getElementById("signup__button");
+
+
+//EVENTS
+document.addEventListener('DOMContentLoaded', transformLabel);
+siginUpBtn.addEventListener("click", validate);
+
+
+function validate(e) {
+  e.preventDefault();
+  //Check that no field is empty
+  inputField.map((field) => {
+    field.value !== "" ? ((userDetails.push(field.value)),
+        (field.style.borderBottom = `solid 3px #3d272a`)) :
+      (field.style.borderBottom = `solid 3px red`);
+  })
+  userDetails.length !== 6 ? userDetails = [] : (window.location.href = "../pages/index.html");
+}
+
+
+function transformLabel() {
+  for (let i = 0; i < inputField.length; i++) {
+    inputField[i].focus();
+    inputLabel[i].style.marginBottom = `21px`;
+    inputLabel[i].style.fontSize = `12px`;
+  }
 }
