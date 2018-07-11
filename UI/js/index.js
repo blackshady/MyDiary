ce9c81914d81bce650df9edee72ada3112ff0e95
Victor Chibuike <victorblackshady@gmail.com>
2018-07-11 12:38:29 +0100

    [finished #158982052] add functionalty to edith post button

--- UI/js/index.js
+++ UI/js/index.js
@@ -7,6 +7,11 @@ const siginUpBtn = document.getElementById("signup__button");
 const loginBtn = document.getElementById("login__button");
 const userMenuIcon = document.getElementsByClassName("menu__user")[0];
 const logouBtn = document.getElementsByClassName("logout__btn")[0];
+const eidthPostBtn = document.getElementsByClassName("edit__post")[0];
+const postField = document.getElementsByClassName("story__text");
+const titleField = document.getElementsByClassName("story__title");
+//const postOutputField = document.getElementsByClassName("story__text")[0];
+
 //EVENTS
 document.addEventListener('DOMContentLoaded', transformLabel);
 
@@ -14,6 +19,7 @@ document.addEventListener('DOMContentLoaded', transformLabel);
 (loginBtn ? (loginBtn.addEventListener("click", validateLogin)) : 0);
 (userMenuIcon ? (userMenuIcon.addEventListener("click", DisplayLogoutBtn)) : 0);
 (logouBtn ? (logouBtn.addEventListener("click", logoutUser)) : 0);
+(eidthPostBtn ? (eidthPostBtn.addEventListener("click", edithPost)) : 0);
 
 // Default action on DOM Load
 function transformLabel() {
@@ -48,7 +54,6 @@ function validateInputs() {
 }
 
 // Logout the user
-
 function DisplayLogoutBtn() {
   logouBtn.style.opacity = 1;
   setTimeout(() => {
@@ -58,4 +63,14 @@ function DisplayLogoutBtn() {
 
 function logoutUser() {
   window.location.href = "../pages/index.html";
+}
+
+// Modify Post
+function edithPost() {
+  let postInField = postField[0];
+  let postOutField = postField[1];
+  let titleInField = titleField[0];
+  let titleOutField = titleField[1];
+  titleInField.value = titleOutField.innerHTML.trim();
+  postInField.innerHTML = postOutField.value;
 }
