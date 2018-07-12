94333105a0f915a288cd30c726dfb7f27935e5b2
Victor Chibuike <victorblackshady@gmail.com>
2018-07-12 23:53:16 +0100

    [#159053462] maake dashboard adjust to screen on resize and refactor code to es6

--- UI/js/index.js
+++ UI/js/index.js
@@ -22,10 +22,135 @@ const siginUpBtn = document.getElementById("signup__button");
 const loginBtn = document.getElementById("login__button");
 const changeNameBtn = document.getElementById("change__name");
 
+let userDetails = [];
 
+// Default action on DOM Load
+let transformItems = () => {
+  inputLabel.forEach((field) => {
+    field.style.marginBottom = `21px`;
+    field.style.fontSize = `12px`;
+  });
+  hideItem(allEntriesBtn);
+  hideItem(profileBtn);
+  hideItem(profileBtn);
+  hideItem(mainAreaBtn);
+};
+
+let hideItem = (item) => (item ? (item.style.display = "none") : 0);
+
+
+// Validate Signup form
+let validateSignup = (e) => {
+  e.preventDefault();
+  validateInputs();
+  userDetails.length !== 6 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
+};
+
+
+// Validate login Form
+let validateLogin = (e) => {
+  e.preventDefault();
+  validateInputs();
+  userDetails.length !== 2 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
+};
+
+// checks if input field is empty
+let validateInputs = () => {
+  inputField.map((field) => {
+    field.value !== "" ? ((userDetails.push(field.value)),
+        (field.style.borderBottom = `solid 3px #3d272a`)) :
+      (field.style.borderBottom = `solid 3px red`);
+  });
+};
+
+let menuClick = 0;
+let DisplayBtn = () => {
+  (menuClick == 1 ? ((hideItem(userMenuIcon.lastElementChild)), (menuClick = 0)) : ((displayGrid(userMenuIcon.lastElementChild)), (menuClick = 1)));
+  userMenuIcon.lastElementChild.style.opacity = 1;
+  if (window.innerWidth <= 730) {
+    displayGrid(mainAreaBtn);
+    displayGrid(profileBtn);
+    displayGrid(allEntriesBtn);
+  }
+};
+
+let displayGrid = (item) => (item ? (item.style.display = "grid") : 0);
+
+
+// Show all the Pane if on dom load
+let showAllPane = () => {
+  if (window.innerWidth > 730) {
+    displayGrid(mainAreaBtn);
+    displayGrid(profileBtn);
+    displayGrid(allEntriesBtn);
+    location.reload();
+  }
+};
+
+let showProfile = () => {
+  displayGrid(leftPane);
+  hideItem(rightPane);
+  hideItem(middlePane);
+};
+
+let showMainArea = () => {
+  displayGrid(middlePane);
+  hideItem(rightPane);
+  hideItem(leftPane);
+};
+
+let showEntries = () => {
+  displayGrid(rightPane);
+  hideItem(middlePane);
+  hideItem(leftPane);
+};
+
+
+// Logout the user
+let logoutUser = () => window.location.href = "../pages/index.html";
+
+
+// Modify Post
+let edithPost = () => {
+  let postInField = postField[0];
+  let postOutField = postField[1];
+  let titleInField = titleField[0];
+  let titleOutField = titleField[1];
+  titleInField.value = titleOutField.innerHTML.trim();
+  postInField.innerHTML = postOutField.value;
+};
+
+// Modify user Details
+let numClick = 0;
+
+let changeUserName = () => {
+  (numClick == 1 ? ((hideItem(profileNameInput)), (numClick = 0)) : ((profileNameInput.style.display = "inline"), (numClick = 1)));
+};
+
+// Reset Password
+let passNumClick = 0;
+
+let resetPass = () => {
+  (passNumClick == 1 ? ((hideItem(restPassInput)), (passNumClick = 0)) : ((restPassInput.style.display = "inline"), (passNumClick = 1)));
+};
+
+// set Reminder
+let remClick = 0;
+
+let setReminder = () => {
+  if (remClick === 1) {
+    hideItem(reminderContent[0]);
+    hideItem(reminderContent[1]);
+    remClick = 0;
+  } else {
+    reminderContent[0].style.display = "inline";
+    reminderContent[1].style.display = "inline";
+    remClick++;
+  }
+};
 
 //EVENTS
-document.addEventListener('DOMContentLoaded', transformLabel);
+document.addEventListener('DOMContentLoaded', transformItems);
 window.addEventListener("resize", showAllPane);
 
 (siginUpBtn ? (siginUpBtn.addEventListener("click", validateSignup)) : 0);
@@ -39,134 +164,3 @@ window.addEventListener("resize", showAllPane);
 (profileBtn ? (profileBtn.addEventListener("click", showProfile)) : 0);
 (allEntriesBtn ? (allEntriesBtn.addEventListener("click", showEntries)) : 0);
 (mainAreaBtn ? (mainAreaBtn.addEventListener("click", showMainArea)) : 0);
-
-let userDetails = [];
-// Default action on DOM Load
-function transformLabel() {
-  for (let i = 0; i < inputField.length; i++) {
-    inputField[i].focus();
-    inputLabel[i].style.marginBottom = `21px`;
-    inputLabel[i].style.fontSize = `12px`;
-  }
-  // button should not display init
-  (allEntriesBtn ? (allEntriesBtn.style.display = "none") : 0);
-  (profileBtn ? (profileBtn.style.display = "none") : 0);
-  (mainAreaBtn ? (mainAreaBtn.style.display = "none") : 0);
-}
-
-// Validate Signup form
-function validateSignup(e) {
-  e.preventDefault();
-  validateInputs();
-  userDetails.length !== 6 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
-}
-
-// Validate login Form
-function validateLogin(e) {
-  e.preventDefault();
-  validateInputs();
-  userDetails.length !== 2 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
-}
-
-// checks if input field is empty
-function validateInputs() {
-  inputField.map((field) => {
-    field.value !== "" ? ((userDetails.push(field.value)),
-        (field.style.borderBottom = `solid 3px #3d272a`)) :
-      (field.style.borderBottom = `solid 3px red`);
-  });
-}
-
-
-function DisplayBtn() {
-  this.lastElementChild.style.opacity = 1;
-  if (window.innerWidth <= 730) {
-    mainAreaBtn.style.display = "grid";
-    profileBtn.style.display = "grid";
-    allEntriesBtn.style.display = "grid";
-  }
-
-
-}
-
-// Show all the Pane if on dom load
-function showAllPane() {
-  if (window.innerWidth > 730) {
-    mainAreaBtn.style.display = "grid";
-    profileBtn.style.display = "grid";
-    allEntriesBtn.style.display = "grid";
-    location.reload();
-  }
-
-}
-
-function showProfile() {
-
-  (middlePane ? middlePane.style.display = "none" : 0);
-  (leftPane ? leftPane.style.display = "grid" : 0);
-  (rightPane ? rightPane.style.display = "none" : 0);
-
-
-}
-
-function showMainArea() {
-
-  (middlePane ? middlePane.style.display = "grid" : 0);
-  (leftPane ? leftPane.style.display = "none" : 0);
-  (rightPane ? rightPane.style.display = "none" : 0);
-
-}
-
-function showEntries() {
-
-  (middlePane ? middlePane.style.display = "none" : 0);
-  (leftPane ? leftPane.style.display = "none" : 0);
-  (rightPane ? rightPane.style.display = "grid" : 0);
-
-}
-
-
-// Logout the user
-function logoutUser() {
-  window.location.href = "../pages/index.html";
-}
-
-// Modify Post
-function edithPost() {
-  let postInField = postField[0];
-  let postOutField = postField[1];
-  let titleInField = titleField[0];
-  let titleOutField = titleField[1];
-  titleInField.value = titleOutField.innerHTML.trim();
-  postInField.innerHTML = postOutField.value;
-}
-
-// Modify user Details
-let numClick = 0;
-
-function changeUserName() {
-  (numClick == 1 ? ((profileNameInput.style.display = "none"), (numClick = 0)) : ((profileNameInput.style.display = "inline"), (numClick = 1)));
-}
-// Reset Password
-let passNumClick = 0;
-
-function resetPass() {
-  (passNumClick == 1 ? ((restPassInput.style.display = "none"), (passNumClick = 0)) : ((restPassInput.style.display = "inline"), (passNumClick = 1)));
-};
-
-// set Reminder
-let remClick = 0;
-
-function setReminder() {
-
-  if (remClick == 1) {
-    reminderContent[0].style.display = "none";
-    reminderContent[1].style.display = "none";
-    remClick = 0;
-  } else {
-    reminderContent[0].style.display = "inline";
-    reminderContent[1].style.display = "inline";
-    remClick++;
-  }
-
-}
