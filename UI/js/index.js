7da624acb1b73bf422b5030294b527fdd95ae3fe
Victor Chibuike <victorblackshady@gmail.com>
2018-07-13 07:16:44 +0100

    [chore #159069981] install winston to dependecy

--- UI/js/index.js
+++ UI/js/index.js
@@ -25,131 +25,131 @@ const changeNameBtn = document.getElementById("change__name");
 let userDetails = [];
 
 // Default action on DOM Load
-let transformItems = () => {
-  inputLabel.forEach((field) => {
-    field.style.marginBottom = `21px`;
-    field.style.fontSize = `12px`;
-  });
-  hideItem(allEntriesBtn);
-  hideItem(profileBtn);
-  hideItem(profileBtn);
-  hideItem(mainAreaBtn);
+const transformItems = () => {
+	inputLabel.forEach((field) => {
+		field.style.marginBottom = `21px`;
+		field.style.fontSize = `12px`;
+	});
+	hideItem(allEntriesBtn);
+	hideItem(profileBtn);
+	hideItem(profileBtn);
+	hideItem(mainAreaBtn);
 };
 
 let hideItem = (item) => (item ? (item.style.display = "none") : 0);
 
 
 // Validate Signup form
-let validateSignup = (e) => {
-  e.preventDefault();
-  validateInputs();
-  userDetails.length !== 6 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
+const validateSignup = (e) => {
+	e.preventDefault();
+	validateInputs();
+	userDetails.length !== 6 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
 };
 
 
 // Validate login Form
-let validateLogin = (e) => {
-  e.preventDefault();
-  validateInputs();
-  userDetails.length !== 2 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
+const validateLogin = (e) => {
+	e.preventDefault();
+	validateInputs();
+	userDetails.length !== 2 ? userDetails = [] : (window.location.href = "../pages/dashboard.html");
 };
 
 // checks if input field is empty
 let validateInputs = () => {
-  inputField.map((field) => {
-    field.value !== "" ? ((userDetails.push(field.value)),
-        (field.style.borderBottom = `solid 3px #3d272a`)) :
-      (field.style.borderBottom = `solid 3px red`);
-  });
+	inputField.map((field) => {
+		field.value !== "" ? ((userDetails.push(field.value)),
+		(field.style.borderBottom = `solid 3px #3d272a`)) :
+			(field.style.borderBottom = `solid 3px red`);
+	});
 };
 
 let menuClick = 0;
-let DisplayBtn = () => {
-  (menuClick == 1 ? ((hideItem(userMenuIcon.lastElementChild)), (menuClick = 0)) : ((displayGrid(userMenuIcon.lastElementChild)), (menuClick = 1)));
-  userMenuIcon.lastElementChild.style.opacity = 1;
-  if (window.innerWidth <= 730) {
-    displayGrid(mainAreaBtn);
-    displayGrid(profileBtn);
-    displayGrid(allEntriesBtn);
-  }
+const DisplayBtn = () => {
+	(menuClick == 1 ? ((hideItem(userMenuIcon.lastElementChild)), (menuClick = 0)) : ((displayGrid(userMenuIcon.lastElementChild)), (menuClick = 1)));
+	userMenuIcon.lastElementChild.style.opacity = 1;
+	if (window.innerWidth <= 730) {
+		displayGrid(mainAreaBtn);
+		displayGrid(profileBtn);
+		displayGrid(allEntriesBtn);
+	}
 };
 
 let displayGrid = (item) => (item ? (item.style.display = "grid") : 0);
 
 
 // Show all the Pane if on dom load
-let showAllPane = () => {
-  if (window.innerWidth > 730) {
-    displayGrid(mainAreaBtn);
-    displayGrid(profileBtn);
-    displayGrid(allEntriesBtn);
-    location.reload();
-  }
+const showAllPane = () => {
+	if (window.innerWidth > 730) {
+		displayGrid(mainAreaBtn);
+		displayGrid(profileBtn);
+		displayGrid(allEntriesBtn);
+		location.reload();
+	}
 };
 
-let showProfile = () => {
-  displayGrid(leftPane);
-  hideItem(rightPane);
-  hideItem(middlePane);
+const showProfile = () => {
+	displayGrid(leftPane);
+	hideItem(rightPane);
+	hideItem(middlePane);
 };
 
-let showMainArea = () => {
-  displayGrid(middlePane);
-  hideItem(rightPane);
-  hideItem(leftPane);
+const showMainArea = () => {
+	displayGrid(middlePane);
+	hideItem(rightPane);
+	hideItem(leftPane);
 };
 
-let showEntries = () => {
-  displayGrid(rightPane);
-  hideItem(middlePane);
-  hideItem(leftPane);
+const showEntries = () => {
+	displayGrid(rightPane);
+	hideItem(middlePane);
+	hideItem(leftPane);
 };
 
 
 // Logout the user
-let logoutUser = () => window.location.href = "../pages/index.html";
+const logoutUser = () => window.location.href = "../pages/index.html";
 
 
 // Modify Post
-let edithPost = () => {
-  let postInField = postField[0];
-  let postOutField = postField[1];
-  let titleInField = titleField[0];
-  let titleOutField = titleField[1];
-  titleInField.value = titleOutField.innerHTML.trim();
-  postInField.innerHTML = postOutField.value;
+const edithPost = () => {
+	const postInField = postField[0];
+	const postOutField = postField[1];
+	const titleInField = titleField[0];
+	const titleOutField = titleField[1];
+	titleInField.value = titleOutField.innerHTML.trim();
+	postInField.innerHTML = postOutField.value;
 };
 
 // Modify user Details
 let numClick = 0;
 
-let changeUserName = () => {
-  (numClick == 1 ? ((hideItem(profileNameInput)), (numClick = 0)) : ((profileNameInput.style.display = "inline"), (numClick = 1)));
+const changeUserName = () => {
+	(numClick == 1 ? ((hideItem(profileNameInput)), (numClick = 0)) : ((profileNameInput.style.display = "inline"), (numClick = 1)));
 };
 
 // Reset Password
 let passNumClick = 0;
 
-let resetPass = () => {
-  (passNumClick == 1 ? ((hideItem(restPassInput)), (passNumClick = 0)) : ((restPassInput.style.display = "inline"), (passNumClick = 1)));
+const resetPass = () => {
+	(passNumClick == 1 ? ((hideItem(restPassInput)), (passNumClick = 0)) : ((restPassInput.style.display = "inline"), (passNumClick = 1)));
 };
 
 // set Reminder
 let remClick = 0;
 
-let setReminder = () => {
-  if (remClick === 1) {
-    hideItem(reminderContent[0]);
-    hideItem(reminderContent[1]);
-    remClick = 0;
-  } else {
-    reminderContent[0].style.display = "inline";
-    reminderContent[1].style.display = "inline";
-    remClick++;
-  }
+const setReminder = () => {
+	if (remClick === 1) {
+		hideItem(reminderContent[0]);
+		hideItem(reminderContent[1]);
+		remClick = 0;
+	} else {
+		reminderContent[0].style.display = "inline";
+		reminderContent[1].style.display = "inline";
+		remClick++;
+	}
 };
 
-//EVENTS
+// EVENTS
 document.addEventListener('DOMContentLoaded', transformItems);
 window.addEventListener("resize", showAllPane);
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,60 +2,60 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './UI/index.js',
-  output: {
-    path: path.resolve(__dirname, './UI/build'),
-    filename: 'bundle.js',
-    // publicPath: './UI/',
-  },
-  module: {
-    rules: [{
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env'],
-        },
-      },
+	entry: './UI/index.js',
+	output: {
+		path: path.resolve(__dirname, './UI/build'),
+		filename: 'bundle.js',
+		// publicPath: './UI/',
+	},
+	module: {
+		rules: [{
+			test: /\.js?$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['env'],
+			},
+		},
 
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [{
-            loader: 'css-loader',
-            options: {
-              modules: false,
-              minimize: true,
-              sourceMap: true,
-              localIdentName: '[hash:base64:5]',
-            },
-          }, ],
-        }),
-      },
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: './fonts/[name].[ext]',
-          },
-        },
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)/i,
-        use: [{
-            loader: 'file-loader',
-            options: {
-              name: './UI/img/[name].[ext]',
-              limit: 10000,
-            },
-          },
-          'img-loader',
-        ],
-      },
-    ],
-  },
+		{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: [{
+					loader: 'css-loader',
+					options: {
+						modules: false,
+						minimize: true,
+						sourceMap: true,
+						localIdentName: '[hash:base64:5]',
+					},
+				}, ],
+			}),
+		},
+		{
+			test: /\.(ttf|otf|eot|woff|woff2)$/,
+			use: {
+				loader: 'file-loader',
+				options: {
+					name: './fonts/[name].[ext]',
+				},
+			},
+		},
+		{
+			test: /\.(png|svg|jpe?g|gif)/i,
+			use: [{
+				loader: 'file-loader',
+				options: {
+					name: './UI/img/[name].[ext]',
+					limit: 10000,
+				},
+			},
+			'img-loader',
+			],
+		},
+		],
+	},
 
-  plugins: [new ExtractTextPlugin('styles.css')],
+	plugins: [new ExtractTextPlugin('styles.css')],
 };
