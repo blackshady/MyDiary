e1185721653cf0868e914ee325a6e12dc1ab0289
Victor Chibuike <victorblackshady@gmail.com>
2018-07-15 14:33:37 +0100

    [chore #159069981] add logger file

---
+++ Server/src/helpers/loger.js
@@ -0,0 +1,14 @@
+import winston from "winston";
+
+const level = process.env.LOG_LEVEL || 'debug';
+
+const logger = new winston.Logger({
+	transports: [
+		new winston.transports.Console({
+			level,
+			timestamp: () => (new Date()).toISOString()
+		})
+	]
+});
+
+export default logger;
