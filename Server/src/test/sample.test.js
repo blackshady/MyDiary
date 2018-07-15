73a1d04aecff5c8663b342b7a2a472ea4b2f4105
Victor Chibuike <victorblackshady@gmail.com>
2018-07-15 15:37:36 +0100

    [finished #159074285] sample.test file passed all sample test

--- Server/src/test/sample.test.js
+++ Server/src/test/sample.test.js
@@ -1,11 +1,13 @@
 import {
   expect
 }
-from "chai";
+from 'chai';
 
 
-describe("sample test", () => {
-  it("check if mocha test is working", () => {
-    expect("hello").to.be.a('string');
+describe('sample test', () => {
+  it('check if mocha test is working', () => {
+    expect('hello').to.be.a('string');
+    expect(123).to.be.a('number');
+    expect('hello').to.have.length(5);
   });
 });
