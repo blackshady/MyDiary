2172dddc893de37fc0c26a6884d998e43dfadf5e
Victor Chibuike <victorblackshady@gmail.com>
2018-07-15 14:16:40 +0100

    [chore #159069981] install nodemon and body-parser

---
+++ Server/src/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import path from "path";
+import logger from "./helpers/logger";
+import config from "./config/config"
+// init app
+const app = express();
+
+//   Middleware
+app.use(bodyParser.json(), bodyParser.urlencoded({
+	extended: false,
+}));
+
+
+app.use(morgan('dev', {
+	skip(req, res) {
+		return res.statusCode < 400;
+	},
+	stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+	skip(req, res) {
+		return res.statusCode >= 400;
+	},
+	stream: process.stdout
+}));
+
+
+// catch all routes
+app.get('/', (req, res) => {
+	res.status(200).sendFile(path.join(__dirname, "index.html"));
+});
+
+// Define Port
+const {
+	development: {
+		app: {
+			port
+		}
+	}
+} = config;
+
+app.listen(port, () => logger.info(`Server started on port ${port}`));
+
+export default app;
