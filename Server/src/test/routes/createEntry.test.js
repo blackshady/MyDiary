87c513be3d0a041bc13c8405ff846336d9ea281e
Victor Chibuike <victorblackshady@gmail.com>
2018-07-20 22:34:28 +0100

    [chore #159210266] add test for create entry endpoint

---
+++ Server/src/test/routes/createEntry.test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import moment from "moment"
+
+import app from '../../app';
+
+describe(' "POST/" create an entry', () => {
+  it('should return a 201 status code', async()=>{
+    const res =  await chai.request(app)
+    .post('/api/v1/entries')
+    .send({
+      "title": "my time an the roof",
+      "story": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nobis aut quisquam aaspernatur eligendi aperiam odit quasi.",
+      "createdAt": moment().format('MMMM DD YYYY, h:mm:s A z').trim()
+    });
+    expect(res).to.have.status(201);
+  });
+});
