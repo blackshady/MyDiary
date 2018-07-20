62904223f4ca06341fbb442c4da7d7deb3f89930
Victor Chibuike <victorblackshady@gmail.com>
2018-07-20 23:35:02 +0100

    [finished #159210266]  test  create entry route

--- Server/src/test/routes/createEntry.test.js
+++ Server/src/test/routes/createEntry.test.js
@@ -16,4 +16,14 @@ describe(' "POST/" create an entry', () => {
     });
     expect(res).to.have.status(201);
   });
+  it('should return a status code of 409 if the fields are empty', async()=>{
+    const res =  await chai.request(app)
+    .post('/api/v1/entries')
+    .send({
+      "title": "",
+      "story": "",
+      "createdAt": moment().format('MMMM DD YYYY, h:mm:s A z').trim()
+    });
+    expect(res).to.have.status(409);
+  });
 });
