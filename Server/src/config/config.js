041f9d9c449c59c307d2c5abc9901f5046523e1e
Victor Chibuike <victorblackshady@gmail.com>
2018-07-23 14:48:52 +0100

    [chore #159047779] change port number

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const config = {
   development: {
     app: {
-      port: parseInt(process.env.DEV_APP_PORT, 10) || 3000,
+      port: parseInt(process.env.PORT, 10) || process.env.DEV_APP_PORT,
     },
   },
   test: {},
