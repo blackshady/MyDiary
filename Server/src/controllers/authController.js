2d0873527faa75fedd703340227f0d70fd04ed0b
Victor Chibuike <victorblackshady@gmail.com>
2018-07-27 14:41:11 +0100

    chore [#159028102] create login route

---
+++ Server/src/controllers/authController.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+/**
+ * @exports
+ * @class AuthController
+ * @description Handles the Sigin and logout of the user
+ */
+class AuthController {
+  /**
+   * Authenticate and Login the User to the application
+   * @async
+   * @param  {object} req - Request object
+   * @param {object} res - Response object
+   * @return {json} Returns json object
+   * @static
+   */
+  static async login(req, res) {
+
+  }
+}
+
+export default AuthController;
