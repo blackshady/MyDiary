1d04ab7b496e40ba217ace94a55b2169dc7c9cbf
Victor Chibuike <victorblackshady@gmail.com>
2018-07-31 13:21:15 +0100

    [feature #159349450] correct file name error

---
+++ Server/src/middlewares/EntriesValidator.js
@@ -0,0 +1,19 @@
+/**
+ * @exports EntriesValidator
+ * @class EntriesValidator
+ * @description Handles all entries validation
+ * 
+ * */
+class EntriesValidator {
+  static validateCreateEntry(req, res, next) {
+    const {
+      title,
+      story
+    } = req.body;
+
+  }
+
+
+}
+
+export default EntriesValidator;
