1bbce6fb22278bab40ca3f019417c5c6dba50127
Victor Chibuike <victorblackshady@gmail.com>
2018-08-01 14:08:45 +0100

    [feature #159346650] add a get entry method

--- Server/src/controllers/EntriesController.js
+++ Server/src/controllers/EntriesController.js
@@ -68,6 +68,37 @@ class EntriesController {
       entries: rows,
     });
   }
+
+  /**
+   * Get a Specific diary entry
+   * @param  {object} req - Request object
+   * @param {object} res - Response object
+   * @return {json} Returns json object
+   * @static
+   */
+  static async getEntry(req, res) {
+    const {
+      entryId,
+    } = req.params;
+    const {
+      userid,
+    } = req.authData;
+
+    const {
+      rows,
+    } = await database.query(find.specificUserDiary, [entryId, userid]);
+    if (rows.length !== 0) {
+      return res.status(200).json({
+        status: 'success',
+        message: ' entry found',
+        dairy: rows[0],
+      });
+    }
+    return res.status(404).json({
+      status: 'error',
+      message: 'Diary Entry can not be found',
+    });
+  }
 }
 
 export default EntriesController;
