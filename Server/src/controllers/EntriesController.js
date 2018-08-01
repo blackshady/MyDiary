3afb4075e23a3df6e64a3e44710b340250b55b6a
Victor Chibuike <victorblackshady@gmail.com>
2018-08-01 10:44:24 +0100

    [feature #159349450] flush out un-used data

--- Server/src/controllers/EntriesController.js
+++ Server/src/controllers/EntriesController.js
@@ -1,7 +1,6 @@
 import moment from 'moment';
 import database from '../config/databaseConnection';
 import find from '../models/queries/find.json';
-import entriesDb from '../models/dummy-db/Entries.json';
 import insert from '../models/queries/insert.json';
 
 
@@ -35,30 +34,6 @@ class EntriesController {
     return res.status(200).json({
       status: 'success',
       entries: rows,
-    });
-  }
-
-  /**
-   * Returns a specific Diary entries
-   * @param  {object} req - Request object
-   * @param {object} res - Response object
-   * @return {json} Returns json object
-   * @static
-   */
-  static getEntry(req, res) {
-    const {
-      entryId,
-    } = req.params;
-    const diary = entriesDb.find(entry => entry.entryId === parseInt(entryId, 10));
-    if (diary) {
-      return res.status(200).json({
-        status: 'success',
-        diary,
-      });
-    }
-    return res.status(404).json({
-      status: 'error',
-      message: 'Diary not found',
     });
   }
 
@@ -98,64 +73,6 @@ class EntriesController {
       status: 'success',
       message: 'entry created successfully',
       entries: rows,
-    });
-  }
-
-  /**
-   * Delete a Diary entry
-   * @param  {object} req - Request object
-   * @param {object} res - Response object
-   * @return {json} Returns json object
-   * @static
-   */
-  static deleteEntry(req, res) {
-    const {
-      entryId,
-    } = req.params;
-    const entryIndex = entriesDb.findIndex(entry => entry.entryId === parseInt(entryId, 10));
-    if (entryIndex !== -1) {
-      entriesDb.splice(entryIndex, 1);
-      return res.status(200).json({
-        status: 'success',
-        message: 'Entry has be deleted successfully',
-        entriesDb,
-      });
-    }
-    return res.status(404).json({
-      status: 'error',
-      message: 'Entry not Found',
-    });
-  }
-
-  /**
-   * update the content of an entry
-   * @param  {object} req - Request object
-   * @param {object} res - Response object
-   * @return {json} Returns json object
-   * @static
-   */
-  static updateEntry(req, res) {
-    const {
-      entryId,
-    } = req.params;
-    const {
-      title,
-      story,
-    } = req.body;
-    const entryIndex = entriesDb.findIndex(entry => entry.entryId === parseInt(entryId, 10));
-    if (entryIndex !== -1) {
-      entriesDb[entryIndex].title = title || entriesDb[entryIndex].story;
-      entriesDb[entryIndex].story = story || entriesDb[entryIndex].story;
-      entriesDb[entryIndex].lastModified = moment().format('MMMM DD YYYY, h:mm:s A z').trim();
-      return res.status(200).json({
-        status: 'success',
-        message: 'Entry updated successfully',
-        entriesDb: entriesDb[entryIndex],
-      });
-    }
-    return res.status(404).json({
-      status: 'error',
-      message: 'Entry not Found',
     });
   }
 }
