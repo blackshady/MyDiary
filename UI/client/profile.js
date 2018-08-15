aaef8e875127097af5cdd9e6c5bb4ab4564afd97
Victor Chibuike <victorblackshady@gmail.com>
2018-08-15 11:58:17 +0100

    feat(client): build functionality to logout user - add logout functionality - make user menu icon a hand pointer [finished #159346547]

---
+++ UI/client/profile.js
@@ -0,0 +1,32 @@
+const logoutBtn = document.getElementsByClassName('btn__logout')[0];
+const userProfileIcon = document.querySelector('.menu__user');
+const userProfileItem = document.getElementById('userProfileItem');
+
+
+const logoutUser = () => {
+  localStorage.removeItem('token');
+  window.location.replace('../pages/index.html');
+};
+
+
+//display User Profile 
+userProfileIcon.addEventListener('click', event => {
+  event.stopPropagation();
+  displayGrid(userProfileItem);
+});
+
+
+// hide user profile
+document.addEventListener('click', event => {
+  if (
+    event.target.id !== 'userProfileItem' ||
+    event.target.id == 'menu__user'
+  ) {
+    hideItem(userProfileItem);
+  }
+});
+
+let hideItem = item => (item ? (item.style.display = 'none') : 0);
+let displayGrid = item => (item ? (item.style.display = 'grid') : 0);
+
+logoutBtn.addEventListener('click', logoutUser)
